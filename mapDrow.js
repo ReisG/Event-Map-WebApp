7298be6e4bd048bd6f06a08631f00a5d6e65bcb1
ReisG <romanryabcev2006@gmail.com>
2023-07-04 00:33:14 +0700

    made web app draw events that were sent to in with url

--- mapDrow.js
+++ mapDrow.js
@@ -1,7 +1,42 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([53.354675, 83.740977], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 var marker = L.marker([51.5, -0.09]).addTo(map);
 marker.bindPopup("<b>Nice Place</b><br>Arthor:Bob<br><a href='https://google.com'>See more</a>")
+
+function moreInfo(eventId)
+{
+    var data = {
+        "web_app_name" : "event_map",
+        "chosen_event_id" : eventId,
+    };
+    tg.sendData(JSON.stringify(data));
+    tg.close();
+}
+
+function drawEvent(event)
+{
+    var marker = L.marker([Number(event.latitude), Number(event.longitude)]).addTo(map);
+    marker.bindPopup(
+        "<b>" + event.name + "</b><br>"
+        + "Организатор: " + event.creator_name + "<br>" 
+        + "Начало: " + event.start_time + "<br>"
+        + "Конец: " + event.end_time + "<br>"
+        + "<button onclick='moreInfo("+event.id+")'>Подробнее</button>"
+    )
+}
+
+let params = new URLSearchParams(location.search);
+var dataGot = params.get("data");
+var parsedDataGot = JSON.parse(dataGot);
+for (var i in parsedDataGot)
+{
+    drawEvent(parsedDataGot[i]);
+}
+
+/*
+Query Example:
+https://..?data={%22N1%22:{%22name%22:%22Hello%22,%20%22latitude%22:%2253.354675%22,%20%22longitude%22:%2283.740977%22,%20%22creator_name%22:%22Bob%22,%20%22start_time%22:%221%22,%20%22end_time%22:%221%22,%20%22id%22:1}}
+*/
