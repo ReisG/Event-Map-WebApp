a86a4ac4f8457ad2fc54ddc975823ebe9642cfe9
ReisG <romanryabcev2006@gmail.com>
2023-07-04 01:35:03 +0700

    Deleted London Mark

--- mapDrow.js
+++ mapDrow.js
@@ -3,8 +3,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-var marker = L.marker([51.5, -0.09]).addTo(map);
-marker.bindPopup("<b>Nice Place</b><br>Arthor:Bob<br><a href='https://google.com'>See more</a>")
+
 
 var sanitizeHTML = function (str) {
 	var temp = document.createElement('div');
