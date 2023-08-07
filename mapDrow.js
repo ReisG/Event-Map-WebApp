47d19acd8582d7278e6fad37281adc2549c0d8ac
Roman <87926234+ReisG@users.noreply.github.com>
2023-08-07 10:06:37 +0700

    getting unix time from bot
    start_time and end_time variables are now in unix time

--- mapDrow.js
+++ mapDrow.js
@@ -24,11 +24,15 @@ function moreInfo(eventId)
 function drawEvent(event)
 {
     var marker = L.marker([Number(event.latitude), Number(event.longitude)]).addTo(map);
+    let start_time = Date(Number(event.start_time));
+    let end_time = Date(Number(event.end_time));
+    let start_time_string = start_time.getHours() + ":" : start_time.setMinutes() + " " + start_time.getDate() + "/" + start_time.getMonth() + "/" + start_time.getFullYear();
+    let end_time_string = end_time.getHours() + ":" : end_time.setMinutes() + " " + end_time.getDate() + "/" + end_time.getMonth() + "/" + end_time.getFullYear();
     marker.bindPopup(
         "<b>" + sanitizeHTML(event.name) + "</b><br>"
         + "Организатор: " + sanitizeHTML(event.creator_name) + "<br>" 
-        + "Начало: " + sanitizeHTML(event.start_time) + "<br>"
-        + "Конец: " + sanitizeHTML(event.end_time) + "<br>"
+        + "Начало: " + start_time_string + "<br>"
+        + "Конец: " + end_time_string + "<br>"
         + "<button onclick='moreInfo("+sanitizeHTML(event.id)+")'>Подробнее</button>"
     )
 }
